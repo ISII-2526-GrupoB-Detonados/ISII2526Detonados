480a72583ce8481a44c22e1f411e8f927e70595f
Alexuss28 <alejandro.jara1@alu.uclm.es>
2025-10-24 13:51:19 +0200

    Añadido GetSelect con Controller y DTO

---
+++ src/AppForSEII2526.API/Controllers/DeviceControllerPurchases.cs
@@ -0,0 +1,34 @@
+﻿using AppForSEII2526.API.DTOs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppForSEII2526.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DeviceControllerPurchases : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        private readonly ILogger<DeviceControllerPurchases> _logger;
+
+        public DeviceControllerPurchases(ApplicationDbContext context, ILogger<DeviceControllerPurchases> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        [ProducesResponseType(typeof(IList<Device_DTO_Comprar>), (int) HttpStatusCode.OK)]
+        public async Task<ActionResult> GetDevicesComprarDTOs(string? color, string? nombre)
+        {
+            var devices = await _context.Devices
+            .Where(d => (color == null || d.Color.Contains(color)) &&
+                   (nombre == null || d.Name.Contains(nombre)))
+            .Select(d=>new Device_DTO_Comprar(d.Id, d.Brand, d.Color, d.Name, d.Model.NameModel, d.PriceForPurchase))
+            .ToListAsync();
+            return Ok(devices);
+        }
+    }
+}
