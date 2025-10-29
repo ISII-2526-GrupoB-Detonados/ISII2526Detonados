ac70407957cc916861a578f48b14260932d117e5
Alejandro Jara <alejandro.jara1@alu.uclm.es>
2025-10-29 08:34:16 +0100

    Añadido GetSelect con Controller y DTO (#74)

---
+++ src/AppForSEII2526.API/DTOs/Device_DTO_Comprar.cs
@@ -0,0 +1,25 @@
+﻿namespace AppForSEII2526.API.DTOs
+{
+    public class Device_DTO_Comprar //mirar más y rquired y key
+    {
+        public Device_DTO_Comprar(int id, string brand, string color, string name, string model, double priceForPurchase)
+        {
+            Id = id;
+            Brand = brand;
+            Color = color;
+            Name = name;
+            Model = model;
+            PriceForPurchase = priceForPurchase;
+        }
+
+
+        //Atributos
+        public int Id { get; set; }
+        public string Brand { get; set; }
+        public string Color { get; set; }
+        public string Name { get; set; }
+        public string Model { get; set; }
+        public double PriceForPurchase { get; set; }
+
+    }
+}
