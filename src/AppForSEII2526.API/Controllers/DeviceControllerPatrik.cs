c242d539ebbf7d80be30e9934190b272021e1ca8
Dante <patrik.lopes@alu.uclm.es>
2025-10-29 20:05:32 +0100

    68 creationcontrollerpruebapatrik (#73)
    * GetSelect añadido con exito a la rama Controller
    
    * Establecido nombre al controller

---
+++ src/AppForSEII2526.API/Controllers/DeviceControllerPatrik.cs
@@ -0,0 +1,53 @@
+﻿using AppForSEII2526.API.DTOs.DevicesDTOrepa;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppForSEII2526.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DeviceController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly ILogger<DeviceController> _logger;
+
+        public DeviceController(ApplicationDbContext context, ILogger<DeviceController> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        /*
+        [HttpGet]
+        [Route("[action]")]
+        [ProducesResponseType(typeof(decimal), (int)HttpStatusCode.OK)]
+        [ProducesResponseType(typeof(string), (int)HttpStatusCode.BadRequest)]
+        public async Task<ActionResult> ComputeDivision(decimal op1, decimal op2)
+        {
+            if (op2 == 0)
+            {
+                _logger.LogError($"{DateTime.Now} Exception: op2=0, division by 0");
+                return BadRequest("op2 must be different from 0");
+            }
+            decimal result = decimal.Round(op1 / op2, 2);
+            return Ok(result);
+        }
+        */
+
+        [HttpGet]
+        [Route("[action]")]
+        [ProducesResponseType(typeof(IList<repairDTOrepa>), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult> GetRepairDTO(string? nombre, string? scaleNombre)
+        {
+            var repair = await _context.Repairs
+                .Include(r => r.Scale) // Importante: incluir la relación Scale
+                .Where(r => (nombre == null || r.Name.Contains(nombre)) &&
+                           (scaleNombre == null || r.Scale.Name.Contains(scaleNombre)))
+                .Select(r => new repairDTOrepa(r.Id, r.Name, r.Description, r.Scale.Name, r.Cost))
+                .ToListAsync();
+            return Ok(repair);
+        }
+
+
+    }
+}
