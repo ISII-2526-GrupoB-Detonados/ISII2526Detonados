1c741971d5050c7f934ffbc0e542d5ec6fb61587
PATRIK LOPES BULHOES DE OLIVEIRA <patrik.lopes@alu.uclm.es>
2025-12-05 14:27:13 +0100

    118 creando state container para repair (#121)
    * Añadido StateContainer de Receipt y añadido state a Program
    
    * Añadida solucion para lanzar web
    
    * Terminado select de receipt
    
    * Select web totalmente funcional
    
    * Cambiado nombre a "Selecciona Reparacion"
    
    * Creado details para receipt
    
    * Details implementado en select para visualizacion de razor
    
    * Concretizados terminos en details
    
    * Traducido al español
    
    * Añadido desplegable para scale
    
    * RetiradoDetails.razor

--- src/AppForSEII2526.Web/Program.cs
+++ src/AppForSEII2526.Web/Program.cs
@@ -1,3 +1,4 @@
+using AppForSEII2526.Web;
 using AppForSEII2526.Web.API;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Identity;
@@ -37,7 +38,17 @@ builder.Services.AddIdentityCore<ApplicationUser>(options => options.SignIn.Requ
 builder.Services.AddSingleton<IEmailSender<ApplicationUser>, IdentityNoOpEmailSender>();
 string? URI2API = builder.Configuration.GetValue(typeof(string), "AppForDevices_API") as string;
 
-builder.Services.AddScoped<ISII2526DetonadosAPIClient>(sp => new ISII2526DetonadosAPIClient(URI2API, new HttpClient()));
+// SOLUCION DE LA IA PARA TIRAR LA WEB
+HttpClientHandler handler = new HttpClientHandler();
+if (builder.Environment.IsDevelopment())
+{
+    handler.ServerCertificateCustomValidationCallback = (message, cert, chain, errors) => true;
+}
+// SOLUCION DE LA IA PARA TIRAR LA WEB
+
+
+builder.Services.AddScoped<ISII2526DetonadosAPIClient>(sp => new ISII2526DetonadosAPIClient(URI2API, new HttpClient(handler)));
+builder.Services.AddScoped<ReceiptStateContainer>();
 
 var app = builder.Build();
 
---
+++ src/AppForSEII2526.Web/ReceiptStateContainer.cs
@@ -0,0 +1,53 @@
+﻿using AppForSEII2526.API.DTOs.ReceiptDTO;
+
+namespace AppForSEII2526.Web
+{
+    public class ReceiptStateContainer
+    {
+        public ReceiptForCreateDTO Receipt { get; private set; } = new ReceiptForCreateDTO()
+        {
+            Repairs = new List<ReceiptItemDTO>()
+        };
+
+        public event Action? OnChange;
+
+        private void NotifyStateChanged() => OnChange?.Invoke();
+
+        public void AddRepairToReceipt(ReceiptItemDTO repairItem)
+        {
+            if (!Receipt.Repairs.Any(ri => ri.RepairName == repairItem.RepairName && ri.ModelToRepair == repairItem.ModelToRepair))
+            {
+                Receipt.Repairs.Add(new ReceiptItemDTO()
+                {
+                    RepairName = repairItem.RepairName,
+                    Scale = repairItem.Scale,
+                    ModelToRepair = repairItem.ModelToRepair,
+                    RepairCost = repairItem.RepairCost
+                });
+            }
+            NotifyStateChanged();
+        }
+
+        public void RemoveRepairItem(ReceiptItemDTO item)
+        {
+            Receipt.Repairs.Remove(item);
+            NotifyStateChanged();
+        }
+
+        public void ClearReceiptCart()
+        {
+            Receipt.Repairs.Clear();
+            NotifyStateChanged();
+        }
+
+        public void ReceiptProcessed()
+        {
+            // Hemos completado el proceso de recibo, creamos un nuevo objeto sin datos
+            Receipt = new ReceiptForCreateDTO()
+            {
+                Repairs = new List<ReceiptItemDTO>()
+            };
+            NotifyStateChanged();
+        }
+    }
+}
