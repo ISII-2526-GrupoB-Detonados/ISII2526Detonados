6b30c993c930f0cfd87d8c83025866b3d4953ab4
Alexuss28 <alejandro.jara1@alu.uclm.es>
2025-12-17 21:33:35 +0100

    Last

--- src/AppForSEII2526.API/Controllers/PurchaseController.cs
+++ src/AppForSEII2526.API/Controllers/PurchaseController.cs
@@ -70,11 +70,12 @@ namespace AppForSEII2526.API.Controllers
                 ModelState.AddModelError("PurchaseItems", "Error! You must include at least one device to be purchased");
 
             //EXAMEN=================================================
-
+            /*
             if (purchaseForCreate.PaymentMethod==PaymentMethod.PayPal)
             {
                 ModelState.AddModelError("PaymentMethod", "¡Error! Solo aceptamos compras pagadas con Tarjeta de Crédito");
             }
+            */
 
             //======================================================
 
